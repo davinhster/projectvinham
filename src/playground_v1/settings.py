1b1e2ca074f64041c8d1c97c3ba4270926ac7ffc
davinhster <vincentnishikino@gmail.com>
2019-12-17 10:55:37 -0800

    Third attempt deployment

--- src/playground_v1/settings.py
+++ src/playground_v1/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'q8%d1q2tnv+$u@fduax*das2s9e2ov(cp6ve$pjg@qkx(xj3h$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
